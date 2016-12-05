b2a1bc1b556d9915a93f146e2e90e9cf781f71be
Jonny Moore <jonnymoore121212@gmail.com>
2016-12-05 12:45:26 +0000

    -F renames variables and refactors to additional question.js file

---
+++ src/question.js
@@ -0,0 +1,46 @@
+function loadQuestion() {
+  var questionIndex = randomQuestionIndex();
+  trueAnswer = data[questionIndex][1];
+  var falseAnswerIndex1 = falseAnswerIndex(questionIndex, -1, -1);
+  var false1 = data[falseAnswerIndex1][1];
+  var falseAnswerIndex2 = falseAnswerIndex(questionIndex, falseAnswerIndex1, -1);
+  var false2 = data[falseAnswerIndex2][1];
+  var falseAnswerIndex3 = falseAnswerIndex(questionIndex, falseAnswerIndex1, falseAnswerIndex2);
+  var false3 = data[falseAnswerIndex3][1];
+  randomizeMultipleChoice(trueAnswer, false1, false2, false3);
+}
+
+function sampleIndex(data) {
+  return Math.floor(Math.random()*data.length);
+}
+
+function randomQuestionIndex() {
+  var questionIndex = sampleIndex(data);
+  var question = '<img src="assets/images/' + data[questionIndex][0].toString() + '"/>'
+  $("#question").html(question);
+  return questionIndex;
+}
+
+function falseAnswerIndex(questionIndex, falseAnswerIndex1, falseAnswerIndex2) {
+  var index = sampleIndex(data);
+  while (index == questionIndex || index == falseAnswerIndex1 || index == falseAnswerIndex2) {
+    index = sampleIndex(data);
+  }
+  return index;
+}
+
+function shuffle(array) {
+    for (let i = array.length; i; i--) {
+        let j = Math.floor(Math.random() * i);
+        [array[i - 1], array[j]] = [array[j], array[i - 1]];
+    }
+  return array
+}
+
+function randomizeMultipleChoice(answer1, answer2, answer3, answer4) {
+  var newOrder = shuffle([answer1, answer2, answer3, answer4]);
+  $("#answer1").html(newOrder[0]);
+  $("#answer2").html(newOrder[1]);
+  $("#answer3").html(newOrder[2]);
+  $("#answer4").html(newOrder[3]);
+}
--- src/quiz.js
+++ src/quiz.js
@@ -2,62 +2,15 @@ $(document).ready(function() {
   loadQuestion();
 });
 
-var answer1 = ''
+var trueAnswer = ''
 var currentQuestionNumber = 1;
 var currentQuestionAnswered = false;
 var score = 0
 const TOTAL_QUESTIONS = 5;
 
-function loadQuestion() {
-  var questionIndex = randomQuestionIndex();
-  answer1 = data[questionIndex][1];
-  var falseAnswerIndex1 = falseAnswerIndex(questionIndex, -1, -1);
-  var answer2 = data[falseAnswerIndex1][1];
-  var falseAnswerIndex2 = falseAnswerIndex(questionIndex, falseAnswerIndex1, -1);
-  var answer3 = data[falseAnswerIndex2][1];
-  var falseAnswerIndex3 = falseAnswerIndex(questionIndex, falseAnswerIndex1, falseAnswerIndex2);
-  var answer4 = data[falseAnswerIndex3][1];
-  randomizeMultipleChoice(answer1, answer2, answer3, answer4);
-}
-
-function sampleIndex(data) {
-  return Math.floor(Math.random()*data.length);
-}
-
-function randomQuestionIndex() {
-  var questionIndex = sampleIndex(data);
-  var question = '<img src="assets/images/' + data[questionIndex][0].toString() + '"/>'
-  $("#question").html(question);
-  return questionIndex;
-}
-
-function falseAnswerIndex(questionIndex, falseAnswerIndex1, falseAnswerIndex2) {
-  var index = sampleIndex(data);
-  while (index == questionIndex || index == falseAnswerIndex1 || index == falseAnswerIndex2) {
-    index = sampleIndex(data);
-  }
-  return index;
-}
-
-function shuffle(array) {
-    for (let i = array.length; i; i--) {
-        let j = Math.floor(Math.random() * i);
-        [array[i - 1], array[j]] = [array[j], array[i - 1]];
-    }
-  return array
-}
-
-function randomizeMultipleChoice(answer1, answer2, answer3, answer4) {
-  var newOrder = shuffle([answer1, answer2, answer3, answer4]);
-  $("#answer1").html(newOrder[0]);
-  $("#answer2").html(newOrder[1]);
-  $("#answer3").html(newOrder[2]);
-  $("#answer4").html(newOrder[3]);
-}
-
 function checkAnswer(){
   var choice = event.target.innerHTML;
-  if (choice == answer1) {
+  if (choice == trueAnswer) {
     score += 1;
   }
   currentQuestionNumber += 1;
