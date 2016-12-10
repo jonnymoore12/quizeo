2c97429ec98683e655047dc715d8c8096370651b
Jonny Moore <jonnymoore121212@gmail.com>
2016-12-10 18:40:48 +0000

    adds basic final_feedback at end of quiz

--- src/quiz.js
+++ src/quiz.js
@@ -3,6 +3,7 @@ var currentQuestionNumber = 1;
 var currentQuestionAnswered = false;
 var score = 0
 var totalQuestions = 5;
+var percentage = 0
 
 $(document).ready(function() {
   getTotalQuestions();
@@ -24,6 +25,7 @@ function checkAnswer(){
       }, 1000);
     } else {
       // presentFinalScore();
+      calculatePercentage();
       finalFeedback();
     }
   }
@@ -44,14 +46,14 @@ function presentFinalScore(){
   alert('Your total score is: ' + score);
 }
 
-// function finalFeedback(){
-//   if (score == totalQuestions) {
-//     alert("Not bad, dad would be proud");
-//   } else {
-//     alert("How can? What would mum say?!");
-//   }
-// }
+function calculatePercentage(){
+  percentage = Math.floor((score / totalQuestions) * 100)
+}
 
 function finalFeedback(){
-  window.location="results.html";
+  document.getElementById("finalFeedback").style.visibility="visible";
+  document.getElementById("totalQuestions").innerHTML=totalQuestions;
+  document.getElementById("correct").innerHTML=score;
+  document.getElementById("percentage").innerHTML=percentage.toString() + "%";
+
 }
